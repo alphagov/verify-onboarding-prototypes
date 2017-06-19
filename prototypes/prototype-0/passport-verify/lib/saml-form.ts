a0a90273ceb708797ab6d07eb2813ae054dab62c
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2017-06-19 14:11:42 +0100

    TT-638: Hide the saml form for 5secs if JS is on
    This prevents the user from seeing a form flash up if the IdP is slow to
    respond to the SAML message.
    
    Also put some shebangs in the shell scripts.
    
    Authors: @richardtowers @georgievh @amritsidhu

--- prototypes/prototype-0/passport-verify/lib/saml-form.ts
+++ prototypes/prototype-0/passport-verify/lib/saml-form.ts
@@ -1,13 +1,16 @@
 export function createSamlForm (location: string, samlRequest: string) {
   return `
-      <h1>Send SAML Authn request to hub</h1>
-			<form method='post' action='${location}'>
-				<input type='hidden' name='samlRequest' value='${samlRequest}'/>
-				<input type='hidden' name='relayState' value=''/>
-				<button>Submit</button>
-			</form>
-			<script>
-				document.forms[0].submit()
-			</script>
-      `
+    <h1>Send SAML Authn request to hub</h1>
+    <form method='post' action='${location}'>
+      <input type='hidden' name='samlRequest' value='${samlRequest}'/>
+      <input type='hidden' name='relayState' value=''/>
+      <button>Submit</button>
+    </form>
+    <script>
+      var form = document.forms[0]
+      form.setAttribute('style', 'display: none;')
+      window.setTimeout(function () { form.removeAttribute('style') }, 5000)
+      form.submit()
+    </script>
+  `
 }
