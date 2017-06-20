a9ee9627fc5c8fb78ecae71211d2707ede2c62bf
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2017-06-20 11:57:07 +0100

    TT-638: Update acceptance tests to test full success journey
    This will change as the stub hub gets more features, but it works for
    now.
    
    Authors: @richardtowers @georgievh

--- prototypes/prototype-0/acceptance-tests/src/test/java/uk/gov/ida/verifyserviceprovider/VerifyServiceProviderAcceptanceTests.java
+++ prototypes/prototype-0/acceptance-tests/src/test/java/uk/gov/ida/verifyserviceprovider/VerifyServiceProviderAcceptanceTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -14,26 +15,26 @@ public class VerifyServiceProviderAcceptanceTests {
 
     @Test
     public void shouldAuthenticateUserSuccessfullyWithoutJavaScript() {
-        WebDriver driver = new HtmlUnitDriver(DesiredCapabilities.chrome()) {{
-            setJavascriptEnabled(false);
-        }};
+        WebDriver driver = new HtmlUnitDriver(DesiredCapabilities.chrome()) {{ setJavascriptEnabled(false); }};
+
         driver.get("http://localhost:3200/verify/start");
 
         assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Send SAML Authn request to hub"));
 
         driver.findElement(By.cssSelector("form>button")).click();
+        driver.findElement(By.id("continue-button")).click();
 
-        assertThat(driver.getTitle(), is("Example Domain"));
+        assertThat(driver.getPageSource(), containsString("Hello Default User you are logged in"));
     }
 
     @Test
     public void shouldAuthenticateUserSuccessfullyWithJavaScript() {
-        WebDriver driver = new HtmlUnitDriver(DesiredCapabilities.chrome()) {{
-            setJavascriptEnabled(true);
-        }};
+        WebDriver driver = new HtmlUnitDriver(DesiredCapabilities.chrome()) {{ setJavascriptEnabled(true); }};
 
         driver.get("http://localhost:3200/verify/start");
-        assertThat(driver.getTitle(), is("Example Domain"));
+        driver.findElement(By.id("continue-button")).click();
+
+        assertThat(driver.getPageSource(), containsString("Hello Default User you are logged in"));
     }
 
 }
