f11da1d8342c6e776d9cabb1fcd7c2d39b90db12
Hristo Georgiev <hristo.georgiev@digital.cabinet-office.gov.uk>
2017-07-03 11:11:48 +0100

    TT-719: Created acceptance tests to verify 'AUTHENTICATION_FAILED' scenario.
    Authors: @tunylund @georgievh

--- prototypes/prototype-0/acceptance-tests/src/test/java/uk/gov/ida/verifyserviceprovider/AccountCreationAcceptanceTests.java
+++ prototypes/prototype-0/acceptance-tests/src/test/java/uk/gov/ida/verifyserviceprovider/AccountCreationAcceptanceTests.java
@@ -10,69 +10,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
-public class VerifyServiceProviderAcceptanceTests {
-
-    @Test
-    public void shouldAuthenticateUserSuccessfullyWithoutJavaScript() {
-        WebDriver driver = new HtmlUnitDriver(DesiredCapabilities.chrome()) {{
-            setJavascriptEnabled(false);
-        }};
-
-        driver.get("http://localhost:3200");
-        driver.findElement(By.cssSelector("#content>a.button.button-start")).click();
-
-        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Send SAML Authn request to hub"));
-        driver.findElement(By.cssSelector("form>button")).click();
-
-        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("You've sent a request."));
-        driver.findElement(By.cssSelector("form>input#continue-button")).click();
-
-        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Select the type of Response"));
-        driver.findElement(By.cssSelector("div>input[value='SUCCESS_MATCH']")).click();
-        driver.findElement(By.cssSelector("input#continue-button")).click();
-
-        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Send a 'SUCCESS_MATCH' Response"));
-
-        driver.findElement(By.name("assertionConsumerServiceUrl")).clear();
-        driver.findElement(By.name("assertionConsumerServiceUrl")).sendKeys("http://localhost:3200/verify/response");
-        driver.findElement(By.name("pid")).clear();
-        driver.findElement(By.name("pid")).sendKeys("billy");
-        driver.findElement(By.cssSelector("input[value='LEVEL_2']")).click();
-        driver.findElement(By.cssSelector("input#continue-button")).click();
-        driver.findElement(By.cssSelector("input#continue-button")).click();
-
-        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Success!"));
-        assertThat(driver.findElement(By.cssSelector("p")).getText(), is("You have successfully logged in as Billy Batson at level of assurance LEVEL_2."));
-    }
-
-    @Test
-    public void shouldAuthenticateUserSuccessfullyWithJavaScript() {
-        WebDriver driver = new HtmlUnitDriver(DesiredCapabilities.chrome()) {{
-            setJavascriptEnabled(true);
-        }};
-
-        driver.get("http://localhost:3200");
-        driver.findElement(By.cssSelector("#content>a.button.button-start")).click();
-
-        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("You've sent a request."));
-        driver.findElement(By.cssSelector("form>input#continue-button")).click();
-
-        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Select the type of Response"));
-        driver.findElement(By.cssSelector("div>input[value='SUCCESS_MATCH']")).click();
-        driver.findElement(By.cssSelector("input#continue-button")).click();
-
-        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Send a 'SUCCESS_MATCH' Response"));
-
-        driver.findElement(By.name("assertionConsumerServiceUrl")).clear();
-        driver.findElement(By.name("assertionConsumerServiceUrl")).sendKeys("http://localhost:3200/verify/response");
-        driver.findElement(By.name("pid")).clear();
-        driver.findElement(By.name("pid")).sendKeys("billy");
-        driver.findElement(By.cssSelector("input[value='LEVEL_2']")).click();
-        driver.findElement(By.cssSelector("input#continue-button")).click();
-
-        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Success!"));
-        assertThat(driver.findElement(By.cssSelector("p")).getText(), is("You have successfully logged in as Billy Batson at level of assurance LEVEL_2."));
-    }
+public class AccountCreationAcceptanceTests {
 
     @Test
     public void shouldCreateUserWithoutJavaScript() {
---
+++ prototypes/prototype-0/acceptance-tests/src/test/java/uk/gov/ida/verifyserviceprovider/AuthenticationFailedAcceptanceTests.java
@@ -0,0 +1,73 @@
+package uk.gov.ida.verifyserviceprovider;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class AuthenticationFailedAcceptanceTests {
+
+    @Test
+    @Ignore
+    public void shouldFailAuthenticationWhenScenarioSelectedWithoutJavaScript() {
+        WebDriver driver = new HtmlUnitDriver(DesiredCapabilities.chrome()) {{
+            setJavascriptEnabled(false);
+        }};
+
+        driver.get("http://localhost:3200");
+        driver.findElement(By.cssSelector("#content>a.button.button-start")).click();
+
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Send SAML Authn request to hub"));
+        driver.findElement(By.cssSelector("form>button")).click();
+
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("You've sent a request."));
+        driver.findElement(By.cssSelector("form>input#continue-button")).click();
+
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Select the type of Response"));
+        driver.findElement(By.cssSelector("div>input[value='AUTHENTICATION_FAILED']")).click();
+        driver.findElement(By.cssSelector("input#continue-button")).click();
+
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Send a 'AUTHENTICATION_FAILED' Response"));
+
+        driver.findElement(By.name("assertionConsumerServiceUrl")).clear();
+        driver.findElement(By.name("assertionConsumerServiceUrl")).sendKeys("http://localhost:3200/verify/response");
+        driver.findElement(By.cssSelector("input#continue-button")).click();
+
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Send SAML Response to RP"));
+        driver.findElement(By.cssSelector("input#continue-button")).click();
+
+
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Authentication failed!"));
+    }
+
+    @Test
+    public void shouldCreateUserWithJavaScript() {
+        WebDriver driver = new HtmlUnitDriver(DesiredCapabilities.chrome()) {{
+            setJavascriptEnabled(true);
+        }};
+
+        driver.get("http://localhost:3200");
+        driver.findElement(By.cssSelector("#content>a.button.button-start")).click();
+
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("You've sent a request."));
+        driver.findElement(By.cssSelector("form>input#continue-button")).click();
+
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Select the type of Response"));
+        driver.findElement(By.cssSelector("div>input[value='AUTHENTICATION_FAILED']")).click();
+        driver.findElement(By.cssSelector("input#continue-button")).click();
+
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Send a 'AUTHENTICATION_FAILED' Response"));
+
+        driver.findElement(By.name("assertionConsumerServiceUrl")).clear();
+        driver.findElement(By.name("assertionConsumerServiceUrl")).sendKeys("http://localhost:3200/verify/response");
+        driver.findElement(By.cssSelector("input#continue-button")).click();
+
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Authentication failed!"));
+    }
+}
